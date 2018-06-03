00f67517d954b2f807a367ed19519ffcbfbb5b0e
odelavia <odelavia@gmail.com>
2018-06-03 00:27:38 -0400

    set up first React-native app ever. Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('albums', () => App);
+const App = () => (
+  <Text>Some Text1!</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
